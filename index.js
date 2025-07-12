e408caca3cf754ee2332d3f033a52e0a876eca8b
Sujeet17m <sujeet17das@gmail.com>
2025-07-12 17:18:32 +0530

    movies db

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
-import { add } from './function.js';
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase , ref ,push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+// console.log(add(10, 20));
 
-console.log(add(10, 20));
+const appSettings = {
+    databaseURL : "https://playground-3fef2-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const moviesInDB = ref(database , "movies")
 
 const inputFieldEl = document.getElementById("input-field");
 const addButtonEl = document.getElementById("add-button");
 
 addButtonEl.addEventListener("click", function () {
     let inputValue = inputFieldEl.value;
-    console.log(inputValue);
+
+    push(moviesInDB ,inputValue)
+
+    console.log(`${inputValue} added to the database`);
 });
 
