662933f07fe459608331c3f6dc91e5658863c222
Sujeet17m <sujeet17das@gmail.com>
2025-07-12 14:45:42 +0530

    an initial step towards firebase

---
+++ index.js
@@ -0,0 +1,8 @@
+const inputFieldEl = document.getElementById("input-field")
+const addBtnEl = document.getElementById("add-button")
+
+addBtnEl.addEventListener("click" , function() {
+    let inputValue = inputFieldEl.value
+    console.log(inputValue);
+    
+})
